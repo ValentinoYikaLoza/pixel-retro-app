ca614bd2cca0c3d6321e92baafe00a33f79a1ef4
ValentinoYikaLoza <138173621+ValentinoYikaLoza@users.noreply.github.com>
2025-08-19 21:19:43 -0500

    ADD: setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixel_retro_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/pixel_retro_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pixel_retro_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
